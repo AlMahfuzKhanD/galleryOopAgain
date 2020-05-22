72d805fe56168cb732689169dfa3844a3947a9f4
mahfuz380 <almahfuz380@gmail.com>
2020-05-22 16:53:40 +0600

    page creation done

---
+++ admin/photos.php
@@ -0,0 +1,54 @@
+<?php include("includes/header.php"); ?>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <!-- top nav -->
+
+            <?php include("includes/top_nav.php"); ?>
+
+
+            <!-- top nav -->
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+            <!-- side nav -->
+
+            <?php include("includes/side_nav.php"); ?>
+
+
+            <!-- side nav -->
+            
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Photos
+                            <small>Editted</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Photos
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+  <?php include("includes/footer.php"); ?>
